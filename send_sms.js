fc946d78ffb6f6af030db6231479c97527c12fea
Rahul Rajan <rahul.rajanus@gmail.com>
2020-03-31 22:33:46 -0400

    sms commit

--- send_sms.js
+++ send_sms.js
@@ -1,21 +1,24 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
+require('dotenv').config();
 
 function sendSMS(messageBody, recipientPhoneNum) {
-
+    console.log("hellooo");
     const accountSid = process.env.ACCOUNT_SID;
     const authToken = process.env.AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
-
+    console.log(accountSid, authToken, messageBody);
+    console.log('+1' + recipientPhoneNum);
     client.messages
     .create({
         body: messageBody,
         from: '+16625544151',
         to: '+1' + recipientPhoneNum
     })
-    .then(message => console.log(message.sid));
+    .then(message => console.log(message.sid)).catch(err => {
+        console.log(err);
+    });
 
 }
-
-export default sendSMS
+module.exports = sendSMS;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var multer  = require('multer');
 require('dotenv').config();
 var sendSMS = require('./send_sms');
 const MongoClient = require("mongodb").MongoClient;
+sendSMS('https://hipaa.jotform.com/200887837659171', '5712260277');
 
 MONGO_URL = "mongodb://127.0.0.1:27017"
 
@@ -15,14 +16,15 @@ MongoClient.connect(
     if (err) return console.log(err);
     db = client.db("staff_trak");
 
-    /* Add code to call Jotform API and refresh data 
+    /* Add code to call Jotform API and refresh data
     in case things changed while the server was down. */
-
+//6am 5pm, way for supervisor to adjust notification times
     app.listen(80, function() {
       console.log("listening on 80");
     });
   }
 );
+
 var app = express();
 var multipart = multer();
 
@@ -35,7 +37,7 @@ app.post('/register', multipart.array(), function(req, res) {
   data = JSON.parse(req.body.rawRequest)
   console.log(data)
 
-  sendSMS('https://hipaa.jotform.com/200887837659171', '5712260277');
+  sendSMS('Thank you for registering!', data.q32_phoneNumber);
 
   db.collection('users').update({phone_number: data.q32_phoneNumber},{
       name: data.q31_name.first + " " + data.q31_name.last,
@@ -53,8 +55,6 @@ app.post('/register', multipart.array(), function(req, res) {
 app.post('/webhook', multipart.array(), function(req, res) {
   // console.log(req.body)
   // res.send(req.body);
-  
-
 });
 //
 // app.listen(port);
