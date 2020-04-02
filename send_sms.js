2cf156682a1c6dc1faf28de834da5a789e5e7b4c
Rahul Rajan <rahul.rajanus@gmail.com>
2020-04-01 20:24:34 -0400

    checkin webhook

--- send_sms.js
+++ send_sms.js
@@ -2,13 +2,21 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 require('dotenv').config();
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+console.log(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken);
+// client.on('tokenAboutToExpire', function() {
+//   // Implement fetchToken() to make a secure request to your backend to retrieve a refreshed access token.
+//   // Use an authentication mechanism to prevent token exposure to 3rd parties.
+//   fetchToken(function(updatedToken) {
+//     client.updateToken(updatedToken);
+//   });
+// });
 
 function sendSMS(messageBody, recipientPhoneNum) {
     console.log("hellooo");
-    const accountSid = process.env.ACCOUNT_SID;
-    const authToken = process.env.AUTH_TOKEN;
-    const client = require('twilio')(accountSid, authToken);
-    console.log(accountSid, authToken, messageBody);
+
     console.log('+1' + recipientPhoneNum);
     client.messages
     .create({
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var multer  = require('multer');
 require('dotenv').config();
 var sendSMS = require('./send_sms');
 const MongoClient = require("mongodb").MongoClient;
-sendSMS('https://hipaa.jotform.com/200887837659171', '5712260277');
+const ObjectID = require('mongodb').ObjectID; 
+// sendSMS('https://hipaa.jotform.com/200887837659171', '5712260277');
 
 MONGO_URL = "mongodb://127.0.0.1:27017"
 
@@ -15,9 +16,9 @@ MongoClient.connect(
   (err, client) => {
     if (err) return console.log(err);
     db = client.db("staff_trak");
-
     /* Add code to call Jotform API and refresh data
     in case things changed while the server was down. */
+
 //6am 5pm, way for supervisor to adjust notification times
     app.listen(80, function() {
       console.log("listening on 80");
@@ -32,14 +33,28 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.get('/', (req,res) => {
     res.send("Hello World");
 })
-app.post('/register', multipart.array(), function(req, res) {
+app.post('/test_register', async function(req,res) {
+    data = req.body
+    response = await db.collection('test-users').findOneAndUpdate({phone_number: data.phoneNumber},{$set: {
+        name: data.name,
+    }},{
+        upsert: true
+    }).then(user => {
+        console.log(user.value._id)
+
+    }).catch(err => {
+        console.log(err);
+    });
+    console.log(response)
+    res.send("ok!")
+})
+app.post('/register', multipart.array(), async function(req, res) {
   console.log(req.body)
   data = JSON.parse(req.body.rawRequest)
   console.log(data)
-
   sendSMS('Thank you for registering!', data.q32_phoneNumber);
 
-  db.collection('users').update({phone_number: data.q32_phoneNumber},{
+  await db.collection('users').findOneAndUpdate({phone_number: data.q32_phoneNumber},{$set: {
       name: data.q31_name.first + " " + data.q31_name.last,
       phone_number: data.q32_phoneNumber,
       department: data.q42_department,
@@ -47,15 +62,31 @@ app.post('/register', multipart.array(), function(req, res) {
       gender: data.q37_gender,
       zipcode: data.q38_homeZip,
       submissions: []
-  },{
+  }},{
       upsert: true
+  }).then(user => {
+      id = user.value._id
+      db.collection('responses').insertOne({
+
+      })
+
   });
   res.send(req.body);
 });
-app.post('/webhook', multipart.array(), function(req, res) {
+app.post('/checkin', multipart.array(), function(req, res) {
   // console.log(req.body)
   // res.send(req.body);
+  data = JSON.parse(req.body.rawRequest)
+  console.log(data)
+
+  db.collection('responses').update({phone_number: data.q32_phoneNumber},{
+      name: data.q31_name.first + " " + data.q31_name.last,
+      phone_number: data.q32_phoneNumber,
+      department: data.q44_department,
+      symptoms: data.q28_whatSymptoms,
+      temperature: data.q43_temperatureIn,
+      exposed_in_last_24h: data.q33_inThe,
+  },{
+      upsert: true
+  });
 });
-//
-// app.listen(port);
-// console.log('Server started! URL: http://localhost:' + port);
