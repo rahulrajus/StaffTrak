66d7bfb449ed15fbca71cd0cde864605b548beea
Shannen Wu <shannen@mit.edu>
2020-04-04 23:07:30 -0400

    modularize db

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// connect to database
+const mongoURL = process.env.MONGO_SRV;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(mongoURL, options);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+// db error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
