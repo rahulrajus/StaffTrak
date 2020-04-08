c58d355a8bf305496181c1aa6ec2b7756bbd224d
Shannen Wu <shannen@mit.edu>
2020-04-07 23:32:43 -0400

    ok merge conflicts fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 import Portal from './portal/Portal';
 import Login from './login/Login';
 import axios from 'axios';
@@ -28,106 +28,18 @@ function App(props) {
 
   const getUser = () => {
     setLoading(false);
-    // setAuthed(true);
-    // axios.get('localhost:80/whoami')
-    //   .then(
-    //     (response) => {
-    //       const userObj = response.data;
-    //       if (userObj._id !== undefined) {
-    //         setUserInfo(userInfo);
-    //         setLoading(false);
-    //       } else {
-    //         setUserInfo(null);
-    //         setLoading(false);
-    //       }
-    //     },
-    //   );
-  }
-
-<<<<<<< HEAD
-const Auth = ({ component: Component, ...rest }) => {
-  const counter = useSelector(state => state);
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        counter ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  );
-}
-
-function App() {
-  const [authed, setAuthed] = useState(false);
-  const [userInfo, setUserInfo] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    document.title = 'StaffTrak';
-    getUser();
-  }, [])
-
-  const getUser = () => {
-    setLoading(false);
-    setAuthed(true);
-    // axios.get('localhost:80/whoami')
-    //   .then(
-    //     (response) => {
-    //       const userObj = response.data;
-    //       if (userObj._id !== undefined) {
-    //         setUserInfo(userInfo);
-    //         setLoading(false);
-    //       } else {
-    //         setUserInfo(null);
-    //         setLoading(false);
-    //       }
-    //     },
-    //   );
   }
 
   return (
-    <div>
-      <Switch>
-        <Redirect from='/logout' to='/login' />
-        <Route exact path='/login' component={Login} />
-        {authed ? <Route exact path='/portal' component={Portal} userInfo={userInfo} /> : <Route exact path='/login' component={Login} />}
-        {/* <PrivateRoute
-          exact
-          path='/portal'
-          authed={authed}
-          loading={loading}
-          userInfo={userInfo}
-          component={Portal}
-        /> */}
-      </Switch>
-    </div>
-=======
-  return (
-    // <div>
-    //   <Switch>
-    //     <Redirect from='/logout' to='/login' />
-    //     <Route exact path='/login' component={Login} />
-    //     {authed ? <Route exact path='/portal' component={Portal} userInfo={userInfo} /> : <Route exact path='/login' component={Login} />}
-    //     {/* <PrivateRoute
-    //       exact
-    //       path='/portal'
-    //       authed={authed}
-    //       loading={loading}
-    //       userInfo={userInfo}
-    //       component={Portal}
-    //     /> */}
-    //   </Switch>
-    // </div>
-    <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens}}>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
       <Router>
-          <Route exact path ='/' component={Login} />
-          <PrivateRoute path = '/portal' component={Portal} />
+        <Route exact path='/' component={Login} />
+        <PrivateRoute path='/portal' component={Portal} />
 
       </Router>
 
     </AuthContext.Provider>
 
->>>>>>> 862267544dff49032ce662b97c59a233d1189951
   );
 }
 
